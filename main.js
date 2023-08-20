90b985005004d0369998cbc8fd682ba063f7b078
tonytor <teme4842@gmail.com>
2023-08-20 16:23:13 +0300

    this is my first portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+  const menu = document.querySelector('.menu-links')
+  const icon = document.querySelector('.hamberger-icon')
+ 
+  menu.classList.toggle('open')
+  icon.classList.toggle('open')
+}
+
+toggleMenu()
